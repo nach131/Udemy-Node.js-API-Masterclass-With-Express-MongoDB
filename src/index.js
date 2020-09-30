89e1d5e5efa315eabce876d52da54b72f00d0482
nacho <nacho.mota@gmail.com>
2020-09-30 11:28:32 +0200

    6_Ejercicio math

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import myCurrentLocation, { getGreating, message, name } from './myModule'
 
+import add, { subtract } from './6_Ejercicio'
 
-console.log(message)
-console.log(name)
-console.log(myCurrentLocation)
-console.log(getGreating('Jessica'))
+// console.log(message)
+// console.log(name)
+// console.log(myCurrentLocation)
+// console.log(getGreating('Jessica'))
 
+console.log(add(4,5))
+console.log(subtract(4,5))
